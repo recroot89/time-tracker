5e483ec9034d1bfe3a9ccd277881db11cfb1d7ca
recroot89 <recroot89@gmail.com>
2019-09-09 16:43:55 +0500

    add main page and not found

--- services/app/app/controllers/application_controller.rb
+++ services/app/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def not_found
+    respond_to do |format|
+      format.html { render template: 'web/rescues/not_found', layout: false, status: :not_found }
+      format.any { head 404 }
+    end
+  end
 end
--- services/app/config/routes.rb
+++ services/app/config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :web do
+    root 'welcome#index' # main page
+  end
+
+  match '*path', to: 'application#not_found', via: :all
 end
